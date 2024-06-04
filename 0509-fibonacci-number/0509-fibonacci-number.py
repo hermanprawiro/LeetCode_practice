3b75fb18624d23ba181acf0e5b8672eb85962613
Herman Prawiro <herman.prawiro@gmail.com>
2024-06-04 14:41:23 +0800

    Time: 26 ms (97.02%), Space: 16.4 MB (75.98%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,13 +1,10 @@
+import functools
+
 class Solution:
-    def fib(self, N: int) -> int:
-        return self.fib_helper(N, {})
-        
-    def fib_helper(self, N: int, memo) -> int:
-        if N == 0:
+    @functools.lru_cache
+    def fib(self, n: int) -> int:
+        if n == 0:
             return 0
-        if N == 1:
+        if n == 1:
             return 1
-        if N in memo:
-            return memo[N]
-        else:
-            return self.fib_helper(N - 1, memo) + self.fib_helper(N - 2, memo)
+        return self.fib(n - 1) + self.fib(n - 2)
