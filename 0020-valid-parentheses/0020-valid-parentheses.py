8cee6c757ba2be67c3632749cd2a03a1e3948307
Herman Prawiro <herman.prawiro@gmail.com>
2024-06-06 16:51:30 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,20 +1,20 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        pair_open = {
-            '(': ')',
-            '[': ']',
-            '{': '}'
+        pairs = {
+            ')': '(',
+            ']': '[',
+            '}': '{'
         }
-        pair_close = {v: k for k, v in pair_open.items()}
 
         stack = []
         for char in s:
-            if char in pair_open:
-                stack.append(char)
-            else:
+            if char in pairs:
                 if len(stack) == 0:
                     return False
                 top = stack.pop()
                 if top != pair_close[char]:
                     return False
+            else:
+                stack.append(char)
+                
         return len(stack) == 0
