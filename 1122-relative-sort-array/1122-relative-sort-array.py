1cfd002e09546cf033ee7b6a92a2b4dab5512cc2
Herman Prawiro <herman.prawiro@gmail.com>
2024-06-11 13:31:06 +0800

    Time: 24 ms (99.66%), Space: 16.6 MB (93.48%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.py
+++ 1122-relative-sort-array/1122-relative-sort-array.py
@@ -2,19 +2,20 @@ class Solution:
     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
         arr2_set = set(arr2)
         dict_count = {i: 0 for i in arr2}
-        key_ex = []
+        # key_ex = []
         result = []
+        result_ex = []
 
         for i in range(len(arr1)):
             v = arr1[i]
             if v in arr2_set:
                 dict_count[v] += 1
             else:
-                key_ex.append(v)
+                result_ex.append(v)
                 dict_count[v] = 1
+        result_ex.sort()
         
-        key_ex = arr2 + sorted(key_ex)
-        for i in range(len(key_ex)):
-            v = key_ex[i]
+        for i in range(len(arr2)):
+            v = arr2[i]
             result = result + [v] * dict_count[v]
-        return result
+        return result + result_ex
