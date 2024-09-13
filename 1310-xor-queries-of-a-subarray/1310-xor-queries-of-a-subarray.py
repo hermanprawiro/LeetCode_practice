fbf17ac8575960ffbd136108c497dda0d8486a56
Herman Prawiro <herman.prawiro@gmail.com>
2024-09-13 10:43:35 +0800

    Time: 311 ms (28.65%), Space: 31.4 MB (10.11%) - LeetHub

--- 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.py
+++ 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.py
@@ -1,19 +1,20 @@
 class Solution:
     def xorQueries(self, arr: List[int], queries: List[List[int]]) -> List[int]:
+        """
+        If we build prefix sum array with len = n, we have to check if left == 0
+        to avoid out of array index error.
+        So, we build prefix sum array with len = n + 1, and put prefix[0] = 0,
+        because x ^ 0 = x
+        """
         n = len(arr)
-        pre = [0] * n
-        pre[0] = arr[0]
+        pre = [0] * (n + 1)
+        pre[1] = arr[0]
         for i in range(1, n):
-            pre[i] = pre[i - 1] ^ arr[i]
+            pre[i + 1] = pre[i] ^ arr[i]
         
         result = []
-        for i, (left, right) in enumerate(queries):
-            if left == right:
-                result.append(arr[right])
-            elif left == 0:
-                result.append(pre[right])
-            else:
-                result.append(pre[left - 1] ^ pre[right])
+        for (left, right) in queries:
+            result.append(pre[left] ^ pre[right + 1])
         return result
 
         
