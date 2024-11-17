968345e52e1ee63f5a12f70062f066a61b77c00b
Herman Prawiro <herman.prawiro@gmail.com>
2024-11-17 12:23:24 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.py
+++ 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.py
@@ -2,12 +2,12 @@ class Solution:
     def resultsArray(self, nums: List[int], k: int) -> List[int]:
         n = len(nums)
         result = [-1] * (n - k + 1)
-        for i in range(n - k + 1):
-            is_valid = True
-            for j in range(k - 1):
-                if nums[i + j + 1] != nums[i + j] + 1:
-                    is_valid = False
-            if is_valid:
-                result[i] = nums[i + k - 1]
+        streak = 1
+        for i in range(n - 1):
+            if nums[i + 1] == (nums[i] + 1):
+                streak += 1
+            else:
+                streak = 1
+            if streak >= k:
+                result[i - k + 2] = nums[i + 1]
         return result
-
