086165b8392027ff8ceaa90a225939368c617312
Herman Prawiro <herman.prawiro@gmail.com>
2024-12-11 14:42:04 +0800

    Time: 239 ms (44.01%), Space: 28.3 MB (30.54%) - LeetHub

--- 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.py
+++ 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.py
@@ -2,9 +2,22 @@ class Solution:
     def maximumBeauty(self, nums: List[int], k: int) -> int:
         # subsequence is not necessarily need to be contiguous
         # that's substring
-        nums.sort()
-        left = 0
-        for right in range(len(nums)):
-            if nums[right] - nums[left] > 2 * k:
-                left += 1
-        return right - left + 1
+        
+        # edge case when len(nums) == 1
+        if len(nums) == 1:
+            return 1
+
+        maxval = max(nums)
+        count = [0] * (maxval + 1)
+        for num in nums:
+            # range start
+            count[max(0, num - k)] += 1 # cap at index 0
+            # range end, +1 because inclusive
+            if num + k + 1 <= maxval: # only if inside [0, maxval] range
+                count[num + k + 1] -= 1
+        answer = 0
+        cursum = 0
+        for val in count:
+            cursum += val
+            answer = max(answer, cursum)
+        return answer
