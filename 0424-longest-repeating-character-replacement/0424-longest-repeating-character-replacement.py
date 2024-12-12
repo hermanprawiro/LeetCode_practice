2378038ff39e2388f7b10d202e9e8a910ad28c61
Herman Prawiro <herman.prawiro@gmail.com>
2024-12-12 15:01:14 +0800

    Time: 101 ms (45.3%), Space: 17.6 MB (15.6%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -5,9 +5,11 @@ class Solution:
         answer = 0
         left = 0
         for right in range(len(s)):
-            c = s[right]
-            count[c] = count.get(c, 0) + 1
-            max_count = max(max_count, count[c])
+            c = s[right] # new char
+            count[c] = count.get(c, 0) + 1 # get count of new char
+            max_count = max(max_count, count[c]) # count of dominant char
+            # the window_size - count of dominant char in the window = count replaceable char
+            # if the replaceable char > k, we reached the current max window size
             if (right - left + 1) - max_count > k:
                 count[s[left]] -= 1
                 left += 1
