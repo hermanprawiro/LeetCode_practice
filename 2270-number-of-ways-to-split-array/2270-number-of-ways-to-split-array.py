3f4666bb7b735fd7268003f7af80dd337278623a
Herman Prawiro <herman.prawiro@gmail.com>
2025-01-03 18:51:05 +0800

    Time: 46 ms (83.3%), Space: 32.2 MB (67.48%) - LeetHub

--- 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.py
+++ 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.py
@@ -1,5 +1,7 @@
 class Solution:
     def waysToSplitArray(self, nums: List[int]) -> int:
+        """
+        # Originally, this can be solved using a prefix array, space comp O(n).
         n = len(nums)
         prefix = [0] * n
         prev = 0
@@ -13,4 +15,19 @@ class Solution:
             right = prefix[-1] - prefix[i]
             if left >= right:
                 answer += 1
+
+        # If we look at the second loop, the prefix[-1] is constant, and
+        # the prefix[i] can be calculated in the second loop.
+        # So, we can improve this to only use 2 int, left_sum and right_sum.
+        # Time comp is the same, space comp become O(1).
+        """
+        left_sum = 0
+        right_sum = sum(nums)
+        answer = 0
+        for i in range(len(nums) - 1):
+            left_sum += nums[i]
+            if left_sum >= (right_sum - left_sum):
+                answer += 1
+        return answer
+
         return answer
